07134f153cf395886de27de98bf97a7e4a74af56
Wooseobee <dntjq0807@naver.com>
2023-12-06 16:51:15 +0900

    init project

---
+++ src/test/java/pollen/pollen_discoveryservice/PollenDiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pollen.pollen_discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollenDiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
